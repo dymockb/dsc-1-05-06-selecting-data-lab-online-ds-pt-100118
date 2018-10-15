4f79c33835f7f17218054203deae31f03723ea16
dymock <sssjbc79@gmail.com>
2018-10-15 22:16:44 +0100

    Add updated files

---
+++ insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Mercury', 'gray', 0, 0.55, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Venus', 'yellow', 0, 0.82, 0);
+
